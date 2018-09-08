f02ebce5c9f9595512cea717dc4ef744fabe7aeb
hasbisevinc <flashasbi@gmail.com>
2018-09-09 01:32:45 +0300

    adding map to ground station

--- ground_station/uiPresenter.js
+++ ground_station/uiPresenter.js
@@ -1,13 +1,13 @@
 window.showRaw = function() {
     var rawContainer = document.getElementById("rawContainer");
-    var mapContainer = document.getElementById("mapContainer");
+    var mapContainer = document.getElementById("map");
     mapContainer.classList.add("hide-container");
     rawContainer.classList.remove("hide-container");
 }
 
 window.showMap = function() {
     var rawContainer = document.getElementById("rawContainer");
-    var mapContainer = document.getElementById("mapContainer");
+    var mapContainer = document.getElementById("map");
     rawContainer.classList.add("hide-container");
     mapContainer.classList.remove("hide-container");
 }
